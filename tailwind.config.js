401610a879b5a0142e0073f58553043028ccf1d5
Muhammad Fadjar <fadzmohd6@gmail.com>
2024-11-05 19:06:54 +0700

    adjusting tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./.{html,js}'],
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
-        },
-        colors: {
-          'booger-green': '#387478',
-        }
-      },
-    },
-    plugins: [],
-  }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
